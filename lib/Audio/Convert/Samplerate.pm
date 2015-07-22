0a33deb3b11526378bd31eefc231161aa04ff44f
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-07-22 15:29:41 +0100

    Basically all the methods in the SRC_STATE object

--- lib/Audio/Convert/Samplerate.pm
+++ lib/Audio/Convert/Samplerate.pm
@@ -15,6 +15,17 @@ class Audio::Convert::Samplerate:ver<v0.0.1>:auth<github:jonathanstowe> {
         }
     }
 
+    class Data is repr('CStruct') {
+        has CArray[num32] $.data-in is rw;
+        has CArray[num32] $.data-out is rw;
+        has int64 $.input-frames is rw;
+        has int64 $.output-frames is rw;
+        has int64 $.input-frames-used is rw;
+        has int64 $.output-frames-gen is rw;
+        has int32 $.end-of-input is rw;
+        has num64 $.src-ratio is rw;
+    }
+
     class State is repr('CPointer') {
 
         sub src_new(int32 $converter-type, int32 $channels, int32 $error) returns State is native('libsamplerate') { * }
@@ -29,6 +40,38 @@ class Audio::Convert::Samplerate:ver<v0.0.1>:auth<github:jonathanstowe> {
             }
 
             $state;
+        }
+
+        sub src_process(State, Data) returns int32 is native('libsamplerate') { * }
+
+        multi method process(Data $data is rw) {
+            my $rc = src_process(self, $data);
+
+            if $rc != 0 {
+                X::ConvertError.new(error-code => $rc).throw;
+            }
+        }
+
+        # put this in here as it simplifies matters
+        sub src_is_valid_ratio (num64 $ratio) returns int32 is native('libsamplerate') { * }
+
+        method is-valid-ratio(Num $ratio) returns Bool {
+            if src_is_valid_ratio($ratio) {
+                True;
+            }
+            else {
+                False;
+            }
+        }
+
+        sub src_set_ratio(State $state, num64 $new_ratio) returns int32 is native('libsamplerate') { * }
+
+        method set-ratio(Num $new-ratio) {
+            my $rc = src_set_ratio(self, $new-ratio);
+
+            if $rc != 0 {
+                X::ConvertError.new(error-code => $rc).throw;
+            }
         }
 
         sub src_reset(State) returns int32 is native('libsamplerate') { * }
@@ -49,16 +92,6 @@ class Audio::Convert::Samplerate:ver<v0.0.1>:auth<github:jonathanstowe> {
 
     }
 
-    class Data is repr('CStruct') {
-        has CArray[num32] $.data-in is rw;
-        has CArray[num32] $.data-out is rw;
-        has int64 $.input-frames is rw;
-        has int64 $.output-frames is rw;
-        has int64 $.input-frames-used is rw;
-        has int64 $.output-frames-gen is rw;
-        has int32 $.end-of-input is rw;
-        has num64 $.src-ratio is rw;
-    }
 
     has State $!state;
 
